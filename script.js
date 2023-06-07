d95d68767cbd7e0e23f1f2c64110c11cc8d447b5
huangmouren2023 <125782788+huangmouren2023@users.noreply.github.com>
2023-06-07 10:35:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-document.getElementById('ask-button').addEventListener('click', function() {
-    var question = document.getElementById('question-input').value;
-    fetch('https://5b0210dd-2041-47d6-be7d-5bf7f20118d6.ai.rpt.dev', {
+document.getElementById('send').addEventListener('click', function() {
+    var input = document.getElementById('input');
+    var message = input.value;
+    input.value = '';
+
+    var chatbox = document.getElementById('chatbox');
+    chatbox.innerHTML += '<p><b>You:</b> ' + message + '</p>';
+
+    fetch('https://4e23b70b-a594-4794-84b7-dbbd37aac3b3.ai.rpt.dev', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({question: question}),
+        body: JSON.stringify({ user_message: message }),
     })
     .then(response => response.json())
     .then(data => {
-        var chatHistory = document.getElementById('chat-history');
-        var newMessage = document.createElement('p');
-        newMessage.textContent = 'You: ' + question;
-        chatHistory.appendChild(newMessage);
-        newMessage = document.createElement('p');
-        newMessage.textContent = 'GPT-3.5: ' + data.answer;
-        chatHistory.appendChild(newMessage);
+        chatbox.innerHTML += '<p><b>GPT-3.5:</b> ' + data.answer + '</p>';
+        chatbox.scrollTop = chatbox.scrollHeight;
     });
 });
 
-document.getElementById('add-conversation-button').addEventListener('click', function() {
-    var initialConversations = document.getElementById('initial-conversations');
-    var newConversation = document.createElement('input');
-    newConversation.type = 'text';
-    initialConversations.appendChild(newConversation);
-});
 
-document.getElementById('clear-database-button').addEventListener('click', function() {
-    fetch('https://5b0210dd-2041-47d6-be7d-5bf7f20118d6.ai.rpt.dev/clear', {
-        method: 'POST',
+document.getElementById('clear').addEventListener('click', function() {
+    fetch('https://4e23b70b-a594-4794-84b7-dbbd37aac3b3.ai.rpt.dev', {
+        method: 'DELETE'
+    })
+    .then(() => {
+        var chatbox = document.getElementById('chatbox');
+        chatbox.innerHTML = '';
     });
 });
