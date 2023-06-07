26fe20d791fb103dfca3b410db73b00dfc7ec8f7
huangmouren2023 <125782788+huangmouren2023@users.noreply.github.com>
2023-06-07 08:14:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 document.getElementById('ask-button').addEventListener('click', function() {
     var question = document.getElementById('question-input').value;
-    // TODO: Send the question to the webhook and display the answer in the chat history
+    fetch('https://5b0210dd-2041-47d6-be7d-5bf7f20118d6.ai.rpt.dev', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({question: question}),
+    })
+    .then(response => response.json())
+    .then(data => {
+        var chatHistory = document.getElementById('chat-history');
+        var newMessage = document.createElement('p');
+        newMessage.textContent = 'You: ' + question;
+        chatHistory.appendChild(newMessage);
+        newMessage = document.createElement('p');
+        newMessage.textContent = 'GPT-3.5: ' + data.answer;
+        chatHistory.appendChild(newMessage);
+    });
 });
 
 document.getElementById('add-conversation-button').addEventListener('click', function() {
-    // TODO: Add a new conversation input
+    var initialConversations = document.getElementById('initial-conversations');
+    var newConversation = document.createElement('input');
+    newConversation.type = 'text';
+    initialConversations.appendChild(newConversation);
 });
 
 document.getElementById('clear-database-button').addEventListener('click', function() {
-    // TODO: Send a request to the webhook to clear the database
+    fetch('https://5b0210dd-2041-47d6-be7d-5bf7f20118d6.ai.rpt.dev/clear', {
+        method: 'POST',
+    });
 });
