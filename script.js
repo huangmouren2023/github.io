209af124cb96505be4b1173b3177f129da96d5fc
huangmouren2023 <125782788+huangmouren2023@users.noreply.github.com>
2023-06-07 08:04:45 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('ask-button').addEventListener('click', function() {
+    var question = document.getElementById('question-input').value;
+    // TODO: Send the question to the webhook and display the answer in the chat history
+});
+
+document.getElementById('add-conversation-button').addEventListener('click', function() {
+    // TODO: Add a new conversation input
+});
+
+document.getElementById('clear-database-button').addEventListener('click', function() {
+    // TODO: Send a request to the webhook to clear the database
+});
